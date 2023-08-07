70a01c33b70d7acce5c129c36a19dc875d3d905e
Léon Delcroix <leondecheux@gmail.com>
2023-08-08 01:40:21 +0200

    rework using database instead of cookies

---
+++ TODO.md
@@ -0,0 +1,11 @@
+# TODO list
+
+ - [x] create endpoint to attempt a word
+ - [x] edit front end to post data to back end 
+ - [] make the front end react to the api response, display error message etc
+ - [] change the load of the page to get the info from the database instead of the cookies
+ - [] create better validation when receiving request
+ - [] study and understand prisma migration mecanism
+ - [] create a development platform on railway, in addition to the prod one
+ - []
+ - []
--- src/routes/api/game/+server.ts
+++ src/routes/api/game/+server.ts
@@ -1,9 +1,16 @@
+import { Prisma } from '@prisma/client';
 import { json } from '@sveltejs/kit';
-import type { RequestHandler } from '@sveltejs/kit';
 import type { RequestEvent } from './$types';
+import { getNocleSutomSolution } from '$lib/server/nocle/nocle_interface';
+import {
+	getCurrentGame,
+	getCurrentGameOrCreateNew,
+	getStartOfDayInFranceAsUTC,
+	getWordMatchingCount,
+	processScore,
+} from '$lib/server/game/game_utils';
 
 class GuessWordRequest {
-	message: string | null = null;
 	word: string;
 
 	constructor(word: string) {
@@ -14,5 +21,53 @@ class GuessWordRequest {
 export const POST = async (requestEvent: RequestEvent) => {
 	const payload: GuessWordRequest = await requestEvent.request.json();
 
-	return json({ status: 'ok' });
+	const session = await requestEvent.locals.getSession();
+	const user = session?.user;
+	if (!user?.id) {
+		throw new Error('User Info Error');
+	}
+
+	const today = new Date();
+	const FranceDate = getStartOfDayInFranceAsUTC();
+
+	// TODO : create a game solution record in the database to avoid spamming their api
+	const solutionWord = await getNocleSutomSolution(today);
+
+	// TODO : merge all checks
+	if (solutionWord[0].toUpperCase() !== payload.word[0].toUpperCase()) {
+		return new Response(JSON.stringify({ error: 'First letter does not match' }), { status: 400 });
+	}
+	if (solutionWord.length != payload.word.length) {
+		return new Response(JSON.stringify({ error: 'Word length does not match' }), { status: 400 });
+	}
+
+	const currentGame = await getCurrentGameOrCreateNew(user.id, FranceDate);
+
+	const scoreForWord = processScore(solutionWord, payload.word);
+	const matchingWordCount = getWordMatchingCount(payload.word, scoreForWord);
+
+	const attempCreated = await prisma.gameAttempt.create({
+		data: {
+			word: payload.word,
+			score: scoreForWord,
+			wordsMatching: matchingWordCount,
+			gameId: currentGame.id,
+		},
+	});
+
+	const result = await prisma.game.update({
+		where: {
+			id: currentGame.id,
+		},
+		data: {
+			attemptCount: currentGame.attemptCount ?? 0 + 1,
+			won: scoreForWord.split('').every((c) => c === 'x'),
+		},
+		include: {
+			attempts: true,
+			user: true,
+		},
+	});
+
+	return json({ game: result });
 };
