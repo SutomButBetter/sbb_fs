7f446b8c5c0c7969c01e784b5b2b4fafd426e9d6
Léon Delcroix <l-delcroix@legallais.com>
2023-08-13 22:24:09 +0200

    test sentry page
    0.2.2

---
+++ src/routes/sentry/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	function handleClick() {
+		throw new Error('TEST FRONT END ERROR');
+	}
+
+	async function handleSubmit(event: Event) {
+		event.preventDefault();
+		await fetch('/sentry', {
+			method: 'GET',
+		});
+	}
+</script>
+
+<button on:click={handleClick}>Throw front end error</button>
+
+<form on:submit={handleSubmit}>
+	<button type="submit">Throw back end error</button>
+</form>
---
+++ src/routes/sentry/+server.ts
@@ -0,0 +1,3 @@
+export const GET = async () => {
+	throw new Error('TEST BACK END ERROR');
+};
