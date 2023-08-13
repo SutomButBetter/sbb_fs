e3214bd0c3683626c746dde65edf007a734b07db
Léon Delcroix <l-delcroix@legallais.com>
2023-08-13 10:05:12 +0200

    added debug log to prisma client generation

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -1,10 +1,30 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = global.prisma || new PrismaClient();
+/**
+ * builds prisma client
+ * 
+ * @returns prisma client
+ */
+function getPrisma() {
+	console.group("PRISMA")
 
-if (process.env.NODE_ENV === 'development') {
-	// used for development to store the prisma client to avoid creating a new connection on each svelte hot-reload
-	global.prisma = prisma;
+	let prismaClient = null;
+	if (!global.prisma) {
+		prismaClient = new PrismaClient();
+		console.debug("Generated new Prisma Client")
+		if (process.env.NODE_ENV === 'development') {
+			// used for development to store the prisma client to avoid creating a new connection on each svelte hot-reload
+			console.debug("DEV: stored prisma client to global variable")
+			global.prisma = prismaClient;
+		}
+	} else {
+		prismaClient = global.prisma;
+		console.debug("DEV: Loaded Prisma Client from global var")
+	}
+
+	console.groupEnd()
+	return prismaClient;
 }
 
+const prisma = getPrisma();
 export { prisma };
