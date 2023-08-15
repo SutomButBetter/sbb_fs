7f7bfbcc485864dcb26c59022192609f46be0395
Léon Delcroix <leondecheux@gmail.com>
2023-08-15 16:35:47 +0200

    SEO attempt

---
+++ src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,26 @@
+export async function GET() {
+	return new Response(
+		`
+      <?xml version="1.0" encoding="UTF-8" ?>
+      <urlset
+        xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:xhtml="https://www.w3.org/1999/xhtml"
+        xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
+        xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
+        xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
+        xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
+      >
+      <url>
+        <loc>https://sbb.leondelcroix.com</loc>
+      </url>
+      <url>
+        <loc>https://sbb.leondelcroix.com/game</loc>
+      </url>
+      </urlset>`.trim(),
+		{
+			headers: {
+				'Content-Type': 'application/xml',
+			},
+		}
+	);
+}
